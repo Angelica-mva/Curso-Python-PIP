82148dde2003de386d28bc9b4c728b7333bd5398
angelica-mva <angelica.mva.ds@gmail.com>
2023-10-02 12:13:26 -0500

    agregué explicación del proyecto

--- web-server/store.py
+++ web-server/store.py
@@ -6,10 +6,10 @@ def get_categories():
     r = requests.get(url_api_categories)
     print (r.status_code)
     print(r.text)
-    print(type(r.text))
-    categories = r.json()
-    print(categories)
-    print(type(categories))
-    for category in categories:
-        print(category['name'])
+    print(type(r.text)) #Hasta aquí el codigo entrega la información pero en FORMATO TEXTO
+    categories = r.json() #Convierte a formato JSON (lista de diccionarios)
+    print(categories) 
+    print(type(categories)) #con este print corroboro que esté en JSON
+    for category in categories: #busca solo el valor de las llaves "name" de cada diccionario
+        print(category['name']) 
 
